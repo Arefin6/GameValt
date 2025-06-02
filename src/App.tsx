723b8b9e22a8cf147072c3e13526ced2e9d12db4
arefin6 <arefinhossain3@gmail.com>
2025-06-02 23:27:46 +0600

    setting up nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Button colorPalette={"blue"}>Click me</Button>
-      <Button>Click me</Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr", // or whatever width you want for aside
+      }}
+    >
+      <GridItem area={"nav"}>
+        <Navbar />
+      </GridItem>
+      {/* <Show when={{ lg: true }}> */}
+      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
+        Aside
+      </GridItem>
+      {/* </Show> */}
+      <GridItem area={"main"} bg={"dodgerblue"}>
+        Content
+      </GridItem>
+    </Grid>
   );
 }
 
