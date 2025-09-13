3f981f61b3827c539d87340dcd65becdcf56f09d
arefin6 <arefinhossain3@gmail.com>
2025-09-13 13:54:46 +0600

    infinite scroll implemnted

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,18 @@
-import UseGame from "@/hooks/useGame";
-import { Box, Button, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
-import GameCard from "./GameCard";
-import GameCardSkeleton from "./GameCardSkeleton";
-import GameCardContainer from "./GameCardContainer";
 import { GameQuery } from "@/App";
+import UseGame from "@/hooks/useGame";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import React from "react";
+import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-  const {
-    data,
-    error,
-    status,
-    isFetching,
-    hasNextPage,
-    fetchNextPage,
-    isFetchingNextPage,
-  } = UseGame(gameQuery);
+  const { data, error, status, hasNextPage, fetchNextPage } =
+    UseGame(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
   const fetchedGamesCount =
     data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
@@ -46,27 +38,25 @@ const GameGrid = ({ gameQuery }: Props) => {
     );
   }
   return (
-    <SimpleGrid padding="10px" columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap={6}>
-      {allGames.length > 0 &&
-        allGames.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
-      <Button
-        onClick={() => fetchNextPage()}
-        disabled={!hasNextPage || isFetchingNextPage}
-        loadingText="Loading more..."
-        size="lg"
-        colorScheme="dark"
+    <InfiniteScroll
+      dataLength={fetchedGamesCount}
+      hasMore={!!hasNextPage}
+      next={() => fetchNextPage()}
+      loader={<Spinner></Spinner>}
+    >
+      <SimpleGrid
+        padding="10px"
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        gap={6}
       >
-        {isFetchingNextPage
-          ? "Loading more..."
-          : hasNextPage
-          ? "Load More Games"
-          : "Nothing more to load"}
-      </Button>
-    </SimpleGrid>
+        {allGames.length > 0 &&
+          allGames.map((game) => (
+            <GameCardContainer key={game.id}>
+              <GameCard game={game} />
+            </GameCardContainer>
+          ))}
+      </SimpleGrid>
+    </InfiniteScroll>
   );
 };
 export default GameGrid;
