634d2e5519b3b9e27f49989c4b0e8ae5741e6271
arefin6 <arefinhossain3@gmail.com>
2025-10-05 12:35:09 +0600

    fixed nav bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,9 +2,11 @@ import useGameQueryStore from "@/store";
 import { Input, InputGroup } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
   return (
     <form
@@ -13,6 +15,7 @@ const SearchInput = () => {
         if (ref.current) {
           setSearchText(ref.current.value);
         }
+        navigate("/");
       }}
     >
       <InputGroup startElement={<BsSearch />}>
